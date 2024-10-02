1ab60ba13b427440732fc51021ad494a2de9ee17
Leonardo062908 <leonardomatheus9155@gmail.com>
2024-10-01 23:34:25 -0300

    Alimentando arquivo page dentro de orbita-de-venus

---
+++ CP2-FRONT-REACT/src/app/calculos/orbita-de-venus/page.tsx
@@ -0,0 +1,12 @@
+// src/app/calculos/orbita-de-venus/page.tsx
+import OrbitaDeVenusContent from "./OrbitaDeVenusContent";
+
+export const metadata = {
+  title: "Cálculo da Órbita de Vênus - Immanuel Velikovsky",
+  description:
+    "Cálculo hipotético da variação da órbita de Vênus baseado nas teorias de Velikovsky.",
+};
+
+export default function OrbitaDeVenusPage() {
+  return <OrbitaDeVenusContent />;
+}
