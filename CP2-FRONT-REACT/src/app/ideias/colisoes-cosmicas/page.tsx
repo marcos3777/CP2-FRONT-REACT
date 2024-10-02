b2ccd9ac77a371e283062daab2587cbdc63993ca
fmelods <felipemelodesousa4@gmail.com>
2024-10-01 23:43:43 -0300

    Iniciando page de Colisões Cósmicas com metadata

---
+++ CP2-FRONT-REACT/src/app/ideias/colisoes-cosmicas/page.tsx
@@ -0,0 +1,7 @@
+// src/app/ideias/colisoes-cosmicas/page.tsx
+import ColisoesCosmicasContent from './ColisoesCosmicasContent';
+
+export const metadata = {
+  title: 'Colisões Cósmicas - Immanuel Velikovsky',
+  description: 'Explorando a teoria das colisões cósmicas proposta por Immanuel Velikovsky.',
+};
