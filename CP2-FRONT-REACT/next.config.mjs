e68cb3450fcdb115783b676f848c7e200e2babce
marcos3777 <marcos_vinicius_11@hotmail.com>
2024-10-01 23:09:30 -0300

    Iniciado projeto

---
+++ CP2-FRONT-REACT/next.config.mjs
@@ -0,0 +1,10 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['apod.nasa.gov', 'images-assets.nasa.gov'], 
+    },
+  };
+  
+  export default nextConfig;
+  
---
+++ CP2-FRONT-REACT/postcss.config.js
@@ -0,0 +1,9 @@
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {
+      config: './tailwind.config.ts',
+    },
+    autoprefixer: {},
+  },
+};
